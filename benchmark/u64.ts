4d249cf29b7c0101d3098db3b21e7f04b34a5dd0
Marcus Pousette <marcus.pousette@quantleaf.com>
2023-01-15 00:05:42 +0100

    Update u64 benchmark

--- benchmark/u64.ts
+++ benchmark/u64.ts
@@ -1,12 +1,13 @@
-import { field, serialize, deserialize, BinaryReader, BinaryWriter } from '../src/index.js'
+import { field, serialize, deserialize } from '../src/index.js'
 import B from 'benchmark'
 import protobuf from "protobufjs";
 
-// Run with "node --loader ts-node/esm ./benchmark/u32.ts"
+// Run with "node --loader ts-node/esm ./benchmark/u64.ts"
 /*
-* json x 3,203,871 ops/sec ±0.36% (96 runs sampled)
-* borsh x 11,025,888 ops/sec ±0.50% (395 runs sampled)
-* protobujs x 9,805,098 ops/sec ±0.34% (395 runs sampled)
+* borsh bigint x 4,188,537 ops/sec ±0.37% (394 runs sampled)
+* protobujs bigint x 7,031,425 ops/sec ±0.26% (396 runs sampled)
+* borsh number x 4,112,788 ops/sec ±0.23% (396 runs sampled)
+* protobujs number x 5,016,302 ops/sec ±0.09% (396 runs sampled)
 */
 
 function getRandomInt(max: number) {
@@ -17,9 +18,9 @@ function getRandomInt(max: number) {
 class Test {
 
     @field({ type: 'u64' })
-    age: bigint;
+    age: bigint | number;
 
-    constructor(age: bigint) {
+    constructor(age: bigint | number) {
         this.age = age;
 
     }
@@ -29,19 +30,36 @@ const ProtoMessage = protoRoot.lookupType("Message");
 const createObject = () => {
     return new Test(BigInt(+new Date))
 }
+const createObjectNumber = () => {
+    return new Test(+new Date)
+}
 const numTestObjects = 10000
-const testObjects = ((new Array(numTestObjects)).fill(createObject()));
-const getTestObject = () => testObjects[getRandomInt(numTestObjects)];
+const testObjectsBigint = ((new Array(numTestObjects)).fill(createObject()));
+const testObjectsNumber = ((new Array(numTestObjects)).fill(createObjectNumber()));
+
+const getTestObjectBigint = () => testObjectsBigint[getRandomInt(numTestObjects)];
+const getTestObjectNumber = () => testObjectsNumber[getRandomInt(numTestObjects)];
+
 const borshArgs = { unchecked: true, object: true }
 
 const suite = new B.Suite()
-suite.add("borsh", () => {
-    deserialize(serialize(getTestObject()), Test, borshArgs)
-}, { minSamples: 300 }).add('protobujs', () => {
-    ProtoMessage.toObject(ProtoMessage.decode(ProtoMessage.encode(getTestObject()).finish()))
-}, { minSamples: 300 }).on('cycle', (event: any) => {
-    console.log(String(event.target));
-}).on('complete', function () {
-    console.log('Fastest is ' + this.filter('fastest').map('name'));
-}).run()
+suite
+    .add("borsh bigint", () => {
+        deserialize(serialize(getTestObjectBigint()), Test, borshArgs)
+    }, { minSamples: 300 }
+    ).add('protobujs bigint', () => {
+        ProtoMessage.toObject(ProtoMessage.decode(ProtoMessage.encode(getTestObjectBigint()).finish()))
+    }, { minSamples: 300 })
+    .add("borsh number", () => {
+        deserialize(serialize(getTestObjectNumber()), Test, borshArgs)
+    }, { minSamples: 300 })
+    .add('protobujs number', () => {
+        ProtoMessage.toObject(ProtoMessage.decode(ProtoMessage.encode(getTestObjectNumber()).finish()))
+    }, { minSamples: 300 }).on('cycle', (event: any) => {
+        console.log(String(event.target));
+    })
+    .on('complete', function () {
+        console.log('Fastest is ' + this.filter('fastest').map('name'));
+    })
+    .run()
 
