7f837dbb3c18a43b42073f7dc88db8a4acf159bd
Marcus Pousette <marcus.pousette@quantleaf.com>
2025-09-02 22:53:35 +0200

    fix: monorepo update

--- src/__tests__/fixtures/enums/a.ts
+++
@@ -1,8 +0,0 @@
-import { deserialize, serialize, variant } from "../../../index.js";
-import { Base } from "./base.js";
-
-
-@variant("A")
-class A extends Base { }
-
-deserialize(serialize(new A()), Base)
--- src/__tests__/fixtures/enums/b.ts
+++
@@ -1,7 +0,0 @@
-import { deserialize, serialize, variant } from "../../../index.js";
-import { Base } from "./base.js";
-
-@variant("B")
-class B extends Base { }
-
-deserialize(serialize(new B()), Base)
